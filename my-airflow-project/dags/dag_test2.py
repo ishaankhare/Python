547bca8920cdff80ece6fde20844d869bf947bb8
ishaankhare <32328865+ishaankhare@users.noreply.github.com>
2023-06-16 14:05:42 +0530

    Add files via upload

---
+++ my-airflow-project/dags/dag_test2.py
@@ -0,0 +1,27 @@
+"""Example DAG demonstrating the usage of the BashOperator."""
+from __future__ import annotations
+
+import datetime
+
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="example_bash_operator_ishaan_2",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["example", "example2"],
+    params={"example_key": "example_value"},
+) as dag:
+    step_a = BashOperator(
+        task_id="step_a",
+        bash_command='echo "$hello_world"',
+        dag=dag,
+    )
+
+    step_a
